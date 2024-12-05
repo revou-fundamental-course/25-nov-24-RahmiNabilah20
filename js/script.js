451546089966b4e98c0b141f68b67441361251d0
Rahmi Nabilah <72597656+RahmiNabilah20@users.noreply.github.com>
2024-12-05 18:16:19 +0700

    update js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
 //ini file javascript
+
+function validateform() {
+    let inputBeratBadan = document.getElementById('berat-badan-input').value;
+    let inputUsia = document.getElementById('usia-input').value;
+    let inputTinggiBadan = document.getElementById('tinggi-badan-input').value;
+
+    console.log('BB:' + inputBeratBadan);
+    console.log('Usia:' + inputUsia);
+    console.log('Tinggi:' + inputTinggiBadan);
+    if (inputBeratBadan != '' && inputUsia != '' && inputTinggiBadan != '') {
+        let calculate = parseInt(inputBeratBadan) + parseInt(inputTinggiBadan);
+        updateResult(calculate);
+    } else {
+        alert('Inputan Anda Kosong! Mohon Cek Kembali');
+    }
+}
+
+function updateResult(value) {
+    document.getElementById('result calculation').innerHTML = value;
+}
